721a2aeb29c205f8bf30418db22bb8450f5f13a3
Nasir Ahmed Momin <nasir_momin@apple.com>
2019-04-25 20:49:34 +0530

    Config File created

---
+++ Survey It/Survey It/SurveyConfig.swift
@@ -0,0 +1,15 @@
+//
+//  SurveyConfig.swift
+//  Survey It
+//
+//  Created by Nasir Ahmed Momin on 25/04/19.
+//  Copyright © 2019 Nasir Ahmed Momin. All rights reserved.
+//
+
+import Foundation
+
+public class SurveyConfig: NSObject {
+    
+    
+    
+}
