a4b0cdf64a556c45e75b4c6cb2c0ba93baa117d3
Nasir Ahmed Momin <nasir_momin@apple.com>
2019-04-25 20:47:22 +0530

    Added New File & Xib in storyboard

---
+++ Survey It/Survey It/SurveyViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SurveyViewController.swift
+//  Survey It
+//
+//  Created by Nasir Ahmed Momin on 25/04/19.
+//  Copyright © 2019 Nasir Ahmed Momin. All rights reserved.
+//
+
+import UIKit
+
+class SurveyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
