7088a18de0c42fc60cec29b82ebca6942e6dcc18
Nasir Ahmed Momin <nasir_momin@apple.com>
2019-05-13 16:08:37 +0530

    New XIb's added

---
+++ Survey It/Survey It/QuestionAnswerTableView.swift
@@ -0,0 +1,15 @@
+//
+//  QuestionAnswerTableView.swift
+//  Survey It
+//
+//  Created by Nasir Ahmed Momin on 13/05/19.
+//  Copyright © 2019 Nasir Ahmed Momin. All rights reserved.
+//
+
+import Foundation
+
+class QuestionAnswerTableView: UITableView {
+    
+    
+    
+}
