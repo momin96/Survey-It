2cab453cf980cb9ca48a46c439d4a619a8713e43
Nasir Ahmed Momin <nasir_momin@apple.com>
2019-09-13 15:38:32 +0530

    Question Answer model object oranised

--- Survey It/Survey It/SurveyConfig.swift
+++ Survey It/Survey It/SurveyConfig.swift
@@ -19,23 +19,52 @@ public class SurveyConfig {
 
 
 public struct QuestionAnswers {
+
+    var question: Question?
+    var answer: Answer?
     
-    var questionIndex: Int?
+    public init(question q: Question, answers ans: Answer) {
+        self.question = q
+        self.answer = ans
+    }
+}
+
+public enum QuestionAnswerType {
+    case A
+}
+
+public struct Question {
     
-    var question: String?
-    var answers: [String]?
-    var correctAnswerIndex: Int?
+    public var questionText: String?
+    public var type: QuestionAnswerType?
     
-    public init(question q: String, answers ans: [String], correctAnswerIndex: Int) {
+    public init(question text: String, type t: QuestionAnswerType? = nil) {
+        self.questionText = text
+        self.type = t
+    }
+}
+
+public struct Answer {
+    
+    public var options: [String]?
+    public var correctAnswer: String?
+    public var selectedAnswer: String?
+    internal var score: Int?
+    
+    public init (options optionList: [String], correctAnswer correct: String) {
+        self.options = optionList
+        self.correctAnswer = correct
+    }
+    
+    var isCorrect: Bool? {
+        guard let selected = selectedAnswer else { return nil }
         
-        if correctAnswerIndex >= ans.count {
-            let e = NSException(name:NSExceptionName(rawValue: "Out of bound") , reason: "Correct Answer index is out of bound", userInfo: nil)
-            e.raise()
-            return
+        guard let correct = correctAnswer else { return nil }
+        
+        if correct == selected {
+            return true
         }
         
-        self.question = q;
-        self.answers = ans
-        self.correctAnswerIndex = correctAnswerIndex
+        return false
     }
 }
